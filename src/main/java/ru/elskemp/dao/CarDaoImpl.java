347de6c72162cb151cb0a1409d99fcd127e10edd
Konstantin Kuznetsov <kskwork@yandex.ru>
2021-06-13 00:17:21 +0300

    init commit

--- src/main/java/ru/elskemp/dao/CarDaoImpl.java
+++ src/main/java/ru/elskemp/dao/CarDaoImpl.java
@@ -11,11 +11,11 @@ public class CarDaoImpl implements CarDao {
     private static final List<Car> carList = new ArrayList<>();
 
     static {
-        carList.add(new Car("Mercedes-Benz M-Класс II (W164) 350", "чёрный", 2007, 650_000.00));
-        carList.add(new Car("Hyundai ix35 I Рестайлинг", "белый", 2015, 945_000));
-        carList.add(new Car("Land Rover Range Rover Evoque I 6-speed", "белый", 2012, 1_490_000));
-        carList.add(new Car("Peugeot 408 I Рестайлинг", "чёрный", 2017, 984_000.00));
-        carList.add(new Car("Volkswagen Touareg II", "серебристый", 2013, 1_797_000));
+        carList.add(new Car("Mercedes-Benz M-class", "black", 2007, 650_000.00));
+        carList.add(new Car("Hyundai ix35", "white", 2015, 945_000));
+        carList.add(new Car("Land Rover Range Rover Evoque", "white", 2012, 1_490_000));
+        carList.add(new Car("Peugeot 408", "black", 2017, 984_000.00));
+        carList.add(new Car("Volkswagen Touareg II", "silver", 2013, 1_797_000));
     }
 
     @Override
